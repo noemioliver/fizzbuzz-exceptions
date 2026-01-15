a1271e69eadd3c5c11d9c7511741933af91309ed
noemioliver <noemioliverborja.com>
2026-01-15 12:22:19 +0100

    añadir pass

---
+++ tests/test-kata.py
@@ -0,0 +1,23 @@
+def test_sale_fizz():
+    pass
+    # si es multiplo de 3 sale fizz
+
+def test_sale_buzz():
+    pass
+    #si es multiplo de 5 sale buzz
+
+def test_sale_fizzbuzz():
+    pass
+    #si el numero es multiplo de 3 y de 5 sale fizzbuzz
+
+def test_numero_cero():
+    pass
+    # si el numero es 0 sale fizzbuzz
+
+def test_numero_sieteydos():
+    pass
+    # si el numero es 7 o 2 te devuelve el texto porque no son divisible ente 3 ni 5
+
+def test_texto():
+    pass
+    #si se pone un texto se verifica que el valueerror detetcte letras
